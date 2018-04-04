9107a73dcd5eee06671869b7e2a8661f336dfda7
Ko Takagi <knockoutmarch@gmail.com>
2018-04-04 17:31:46 +0900

    commit scaffold plugin

---
+++ bogo-date.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Bogo Date
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     bogo-date
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Bogo_Date
+ */
+
+// Your code starts here.
