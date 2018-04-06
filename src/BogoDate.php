74d8f69cc47cbff529d498eb4408a3ffbe90732e
Ko Takagi <knockoutmarch@gmail.com>
2018-04-06 16:25:36 +0900

    upgrade

---
+++ src/BogoDate.php
@@ -0,0 +1,62 @@
+<?php
+
+class BogoDate
+{
+	private $prefix = 'bogodate';
+
+	public function __construct() {
+	}
+
+    public static function get_instance() {
+        static $instance;
+        if ( ! $instance ) {
+            $instance = new BogoDate();
+        }
+        return $instance;
+    }
+
+	public function register() {
+		add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ) );
+		add_filter( 'get_the_date', array( $this, 'get_the_date' ) );
+		add_filter( 'get_the_time', array( $this, 'get_the_time' ) );
+	}
+
+    public function plugins_loaded() {
+        if ( is_admin() ) {
+			BogoDate\Admin::get_instance()->register();
+        }
+    }
+
+    public function get_the_date( $the_date, $d, $post ) {
+        $format = $this->get_option( 'date_format_' . get_locale() );
+
+        if ( $format ) {
+            $the_date = date_i18n( $format, strtotime( $the_date ) );
+        }
+
+        return $the_date;
+    };
+
+    public function get_the_time( $the_time, $d, $post ) {
+        $format = $this->get_option( 'time_format_' . get_locale() );
+
+        if ( $format ) {
+            $the_time = get_post_time( $format, false, $post, true );
+        }
+
+        return $the_time;
+    };
+
+	public function get_prefix() {
+		return $this->prefix;
+	}
+
+	public function get_option( $key, $default = null ) {
+		$option = get_option( $this->get_prefix(), array() );
+		if ( ! empty( $option[ $key ] ) && trim( $option[ $key ] ) ) {
+			return trim( $option[ $key ] );
+		} else {
+			return $default;
+		}
+	}
+}
